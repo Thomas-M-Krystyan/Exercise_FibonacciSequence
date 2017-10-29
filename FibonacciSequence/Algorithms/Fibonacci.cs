53d39ac7e20e1f333d2679ad7f161fc26a455668
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-10-29 16:56:48 +0100

    Add new fifth algorithm -> recursive memorization, based on F(n) = F(n-1) + F(n-2) formula

--- FibonacciSequence/Algorithms/Fibonacci.cs
+++ FibonacciSequence/Algorithms/Fibonacci.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FibonacciSequence.Algorithms
 {
     internal static class Fibonacci
     {
     #region Displaying Fibonacci sequence
-        // Iteration method #1 (using temporary variable)
+        // Iteration method #1 (using inner variables, incrementation, and temporary variable)
         internal static void GenerateSequence1(ulong iterationNumber)
         {
             // Displaying variable
@@ -23,7 +24,7 @@ namespace FibonacciSequence.Algorithms
             }
         }
 
-        // Iteration method #2 (using temporary variable)
+        // Iteration method #2 (using outer variables, decrementation, and temporary variable)
         internal static void GenerateSequence2(ulong iterationNumber)
         {
             // Displaying variable
@@ -34,16 +35,19 @@ namespace FibonacciSequence.Algorithms
             ulong prevFibonacci = 1UL;
             ulong currentFibonacci;
 
-            while (iterationNumber-- > 0UL)
+            ulong counter = iterationNumber;
+            while (counter > 0UL)
             {
-                Console.WriteLine($"{displayCounter}. {prevPrevFibonacci}");
+                Console.WriteLine(displayCounter + ". " + prevPrevFibonacci);
                 currentFibonacci = prevPrevFibonacci;  // Temporary variable
                 prevPrevFibonacci = prevFibonacci;
                 prevFibonacci += currentFibonacci;
 
                 displayCounter++;
+                counter--;
             }
-            Console.WriteLine($"{displayCounter}. {prevPrevFibonacci}");
+            // Display the last element
+            Console.WriteLine(displayCounter + ". " + prevPrevFibonacci);
         }
 
         // Recursion method #1 (using substitution)
@@ -55,7 +59,6 @@ namespace FibonacciSequence.Algorithms
             {
                 GenerateSequence3(iterationNumber: iterationNumber, counter: counter + 1UL,
                                   prevPrevFibonacci: prevFibonacci, prevFibonacci: prevPrevFibonacci + prevFibonacci);
-                
             }
         }
 
@@ -64,6 +67,7 @@ namespace FibonacciSequence.Algorithms
         {
             switch (number)
             {
+                // Return Fibonacci constants: 0 or 1 immediately
                 case 0UL:
                     return 0UL;
                 case 1UL:
@@ -73,50 +77,86 @@ namespace FibonacciSequence.Algorithms
             }
         }
 
-        // Upgraded recursion method #3 (using "memorize strategy")
-        internal static ulong GenerateSequence5(ulong number)
-        {
-            switch (number)
+        #region Memorize recursion
+            private static readonly Dictionary<ulong, ulong> MemorizedResults = new Dictionary<ulong, ulong>();
+
+            // Recursion method #3 (upgraded version by using "memorize strategy")
+            internal static ulong GenerateSequence5(ulong number)
             {
-                case 0UL:
-                    return 0UL;
-                case 1UL:
-                    return 1UL;
-                default:
-                    return GenerateSequence4(number - 1UL) + GenerateSequence4(number - 2UL);
+                // Return Fibonacci constants: 0 or 1 immediately
+                switch (number)
+                {
+                    case 0UL:
+                        return 0UL;
+                    case 1UL:
+                        return 1UL;
+                    default:
+                        return ReturnFibonacci(number - 1UL) + ReturnFibonacci(number - 2UL);
+                }
             }
-        }
+
+            // Method to memorize results of Fibonacci calculations
+            private static ulong ReturnFibonacci(ulong number)
+            {
+                if (MemorizedResults.ContainsKey(number))
+                {
+                    return MemorizedResults[number];
+                }
+                ulong result = GenerateSequence5(number);
+                MemorizedResults.Add(number, result);
+
+                return result;
+            }
+        #endregion
     #endregion
 
     #region Returning n-th element of Fibonacci sequence
-        // Iteration method #1 (using temporary variable)
+        // Iteration method #1 (using inner variables, incrementation, and temporary variable)
         internal static string Return_Nth_Element1(ulong iterationNumber)
         {
-            string result = default(string);
+            // Return Fibonacci constants: 0 or 1 immediately
+            switch (iterationNumber)
+            {
+                case 0UL:
+                    return Utilities.HelperMethods.DisplayResult(iterationNumber: iterationNumber, fibonacciNumber: 0UL);
+                case 1UL:
+                    return Utilities.HelperMethods.DisplayResult(iterationNumber: iterationNumber, fibonacciNumber: 1UL);
+            }
 
             // Fibonacci variables & algorithm
             for (ulong i = 0UL, prevPrevFibonacci = 0UL, prevFibonacci = 1UL, currentFibonacci; i <= iterationNumber; i++)
             {
                 if (i == iterationNumber)
                 {
-                    result = Utilities.HelperMethods.DisplayResult(iterationNumber: iterationNumber,
-                                                                   fibonacciNumber: prevPrevFibonacci);
+                    return Utilities.HelperMethods.DisplayResult(iterationNumber: iterationNumber,
+                                                                 fibonacciNumber: prevPrevFibonacci);
                 }
                 currentFibonacci = prevPrevFibonacci + prevFibonacci;  // Temporary variable
                 prevPrevFibonacci = prevFibonacci;
                 prevFibonacci = currentFibonacci;
             }
 
-            return result;
+            return default(string);
         }
 
-        // Iteration method #2 (using temporary variable)
+        // Iteration method #2 (using outer variables, decrementation, and temporary variable)
         internal static string Return_Nth_Element2(ulong iterationNumber)
         {
             // Fibonacci variables
             ulong prevPrevFibonacci = 0UL;
             ulong prevFibonacci = 1UL;
             ulong currentFibonacci;
+
+            // Return Fibonacci constants: 0 or 1 immediately
+            switch (iterationNumber)
+            {
+                case 0UL:
+                    return Utilities.HelperMethods.DisplayResult(iterationNumber: iterationNumber,
+                                                                 fibonacciNumber: prevPrevFibonacci);
+                case 1UL:
+                    return Utilities.HelperMethods.DisplayResult(iterationNumber: iterationNumber,
+                                                                 fibonacciNumber: prevFibonacci);
+            }
 
             ulong counter = iterationNumber;
             while (counter > 0UL)
